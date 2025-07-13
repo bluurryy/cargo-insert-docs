20ddd8c61916f0e62e5a4d22dbcaa65791653ff8
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-07-13 17:48:09 +0200

    refactor: `extract_crate_docs`

--- src/extract_crate_docs.rs
+++ src/extract_crate_docs.rs
@@ -2,7 +2,7 @@ mod child_to_parent;
 
 use std::{collections::HashMap, fs};
 
-use cargo_metadata::PackageId;
+use cargo_metadata::{Metadata, PackageId};
 use color_eyre::eyre::{Context as _, OptionExt as _, Report, Result, bail, eyre};
 use nonempty_collections::NEVec;
 use rustdoc_json::Color;
@@ -13,76 +13,18 @@ use tracing::warn;
 use crate::{Context, markdown};
 
 pub fn extract(cx: &Context) -> Result<String> {
-    let crate_to_package: HashMap<String, &PackageId> =
-        cx.metadata.packages.iter().map(|p| (p.name.as_ref().replace('-', "_"), &p.id)).collect();
+    let json = create_rustdoc_json(cx)?;
+    let krate = parse_rustdoc_json(&json)?;
 
-    let krate = rustdoc_json(cx)?;
-    let root = krate.index.get(&krate.root).ok_or_eyre("crate index has no root")?;
-    let docs = root.docs.as_deref().unwrap_or("").to_string();
-
-    let resolver = Resolve {
-        cx,
+    extract_docs(ExtractDocsOptions {
         krate: &krate,
-        child_to_parent: child_to_parent::child_to_parent(&krate)?.into_iter().collect(),
-        path_to_kind: path_to_kind_map(&krate),
-        crate_to_package,
-    };
-
-    let mut new_docs = docs.clone();
-
-    for link in markdown::links(&docs).into_iter().rev() {
-        let markdown::Link { span, link_type: _, dest_url, title, id: _, content_span } = link;
-
-        let Some(&item_id) = root.links.get(&*dest_url) else {
-            // rustdoc has no item for this url
-            // the link could just not be rustdoc related like `https://www.rust-lang.org/` or
-            // the link is dead because some feature is not enabled or code is cfg'd out
-            // we keep such links as they are
-            continue;
-        };
-
-        let url = match resolver.item_url(item_id) {
-            Ok(ok) => Some(ok),
-            Err(err) => {
-                warn!(cause=%err, link=%dest_url, "failed to resolve doc link");
-                None
-            }
-        };
-
-        let content = &docs[content_span.clone().unwrap_or(0..0)];
-
-        let replace_with = match url {
-            Some(mut url) => {
-                // You can link to sections within an item's documentation by writing `[Vec](Vec#guarantees)`.
-                if let Some(hash) = dest_url.find("#") {
-                    url.push_str(&dest_url[hash..]);
-                }
-
-                use std::fmt::Write;
-                let mut s = String::new();
-
-                write!(s, "[{content}]({url}").unwrap();
-
-                if !title.is_empty() {
-                    write!(s, " \"{title}\"").unwrap();
-                }
-
-                write!(s, ")").unwrap();
-                s
-            }
-            None => content.to_string(),
-        };
-
-        new_docs.replace_range(span, &replace_with);
-    }
-
-    let new_docs = markdown::clean_code_blocks(&new_docs);
-    let new_docs = markdown::shrink_headings(&new_docs);
-
-    Ok(new_docs)
+        metadata: &cx.metadata,
+        on_not_found: &mut |link, cause| warn!(%cause, %link, "failed to resolve doc link"),
+        link_to_latest: cx.args.link_to_latest,
+    })
 }
 
-fn rustdoc_json(cx: &Context) -> Result<Crate> {
+fn create_rustdoc_json(cx: &Context) -> Result<String> {
     let mut builder = rustdoc_json::Builder::default()
         .toolchain(&cx.args.toolchain)
         .manifest_path(&cx.args.manifest_path)
@@ -129,9 +71,7 @@ fn rustdoc_json(cx: &Context) -> Result<Crate> {
         builder.build()?
     };
 
-    let json = fs::read_to_string(json_path).context("failed to read generated rustdoc json")?;
-
-    parse_rustdoc_json(&json)
+    fs::read_to_string(json_path).context("failed to read generated rustdoc json")
 }
 
 fn parse_rustdoc_json(rustdoc_json: &str) -> Result<Crate, Report> {
@@ -162,7 +102,86 @@ fn parse_rustdoc_json(rustdoc_json: &str) -> Result<Crate, Report> {
     serde_json::from_str(rustdoc_json).context("failed to parse generated rustdoc json")
 }
 
-fn path_to_kind_map(krate: &Crate) -> HashMap<&[String], ItemKind> {
+struct ExtractDocsOptions<'a> {
+    krate: &'a Crate,
+    metadata: &'a Metadata,
+    on_not_found: &'a mut dyn FnMut(&str, Report),
+    link_to_latest: bool,
+}
+
+fn extract_docs(
+    ExtractDocsOptions { krate, metadata, on_not_found, link_to_latest }: ExtractDocsOptions,
+) -> Result<String, Report> {
+    let crate_to_package: HashMap<String, &PackageId> =
+        metadata.packages.iter().map(|p| (p.name.as_ref().replace('-', "_"), &p.id)).collect();
+
+    let root = krate.index.get(&krate.root).ok_or_eyre("crate index has no root")?;
+    let docs = root.docs.as_deref().unwrap_or("").to_string();
+
+    let resolver = Resolver {
+        metadata,
+        krate,
+        child_to_parent: child_to_parent::child_to_parent(krate)?.into_iter().collect(),
+        path_to_kind: path_to_item_kind_map(krate),
+        crate_to_package,
+        link_to_latest,
+    };
+
+    let mut new_docs = docs.clone();
+
+    for link in markdown::links(&docs).into_iter().rev() {
+        let markdown::Link { span, link_type: _, dest_url, title, id: _, content_span } = link;
+
+        let Some(&item_id) = root.links.get(&*dest_url) else {
+            // rustdoc has no item for this url
+            // the link could just not be rustdoc related like `https://www.rust-lang.org/` or
+            // the link is dead because some feature is not enabled or code is cfg'd out
+            // we keep such links as they are
+            continue;
+        };
+
+        let url = match resolver.item_url(item_id) {
+            Ok(ok) => Some(ok),
+            Err(err) => {
+                on_not_found(&dest_url, err);
+                None
+            }
+        };
+
+        let content = &docs[content_span.clone().unwrap_or(0..0)];
+
+        let replace_with = match url {
+            Some(mut url) => {
+                // You can link to sections within an item's documentation by writing `[Vec](Vec#guarantees)`.
+                if let Some(hash) = dest_url.find("#") {
+                    url.push_str(&dest_url[hash..]);
+                }
+
+                use std::fmt::Write;
+                let mut s = String::new();
+
+                write!(s, "[{content}]({url}").unwrap();
+
+                if !title.is_empty() {
+                    write!(s, " \"{title}\"").unwrap();
+                }
+
+                write!(s, ")").unwrap();
+                s
+            }
+            None => content.to_string(),
+        };
+
+        new_docs.replace_range(span, &replace_with);
+    }
+
+    let new_docs = markdown::clean_code_blocks(&new_docs);
+    let new_docs = markdown::shrink_headings(&new_docs);
+
+    Ok(new_docs)
+}
+
+fn path_to_item_kind_map(krate: &Crate) -> HashMap<&[String], ItemKind> {
     let mut map = HashMap::new();
 
     for item_summary in krate.paths.values() {
@@ -172,15 +191,16 @@ fn path_to_kind_map(krate: &Crate) -> HashMap<&[String], ItemKind> {
     map
 }
 
-struct Resolve<'a> {
-    cx: &'a Context<'a>,
+struct Resolver<'a> {
+    metadata: &'a Metadata,
     krate: &'a Crate,
     child_to_parent: HashMap<Id, Id>,
     path_to_kind: HashMap<&'a [String], ItemKind>,
     crate_to_package: HashMap<String, &'a PackageId>,
+    link_to_latest: bool,
 }
 
-impl Resolve<'_> {
+impl Resolver<'_> {
     fn item_url(&self, id: Id) -> Result<String> {
         let path = self.item_path(id)?.into_iter().rev().collect();
         let path = fuse_impl_function_to_method(path);
@@ -211,12 +231,12 @@ impl Resolve<'_> {
         if matches!(name, "core" | "alloc" | "std") {
             format!("https://doc.rust-lang.org/{name}/")
         } else {
-            let metadata = &self.cx.metadata;
+            let metadata = &self.metadata;
             let package_id = self.crate_to_package.get(name);
             let package = package_id.map(|&p| &metadata[p]);
             let package_name = package.map(|p| p.name.as_str()).unwrap_or(name);
             let from_workspace = package_id.map(|&p| metadata.workspace_members.contains(p));
-            let link_to_latest = self.cx.args.link_to_latest && from_workspace.unwrap_or(false);
+            let link_to_latest = self.link_to_latest && from_workspace.unwrap_or(false);
 
             let version = if let Some(package) = package
                 && !link_to_latest
