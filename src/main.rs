70410814afc574d3d0e5bba3623d2bf95f243380
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-08-16 00:14:07 +0200

    add config test crate

--- src/main.rs
+++ src/main.rs
@@ -389,8 +389,12 @@ fn try_main(args: &ArgsConfig, log: &PrettyLog) -> Result<()> {
 
         let cfg_patch = config::read_package_config(&toml)?;
 
+        dbg!(&cfg_patch);
+
         let cfg =
             workspace_package_config_patch.apply(&cfg_patch).apply(&args.package_patch).finish();
+
+        dbg!(&cfg);
 
         let enabled_features =
             cfg.features.iter().filter(|&f| package.features.contains_key(f)).cloned().collect();
